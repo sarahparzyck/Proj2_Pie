958f4df5d2b923158ef8585db27594db94e2ae9f
Sarah Parzyck <sarahparzyck@gmail.com>
2020-09-12 18:44:48 -0500

    mapping cause and state

--- plots.js
+++ plots.js
@@ -1,104 +1,88 @@
 // Submit Button handler
-// function GetURL() {
 
-
-//   //Use D3 to select the dropdown menu
-//   var dropdownMenu = d3.selectAll("#dropDown1").node();
-//   // Assign the dropdown menu option to a variable
-//   var selectedOption = dropdownMenu.value;
-//   console.log(selectedOption);
-//   // // Create the plots based on selections
-//   // BuildPie(selectedOption);
-//   };
-
-function CreateURL(state, year) {
+async function CreateURL(state, year) {
   var state = document.getElementById('dropDown1').value;
   var year = document.getElementById('dropDown2').value;
   console.log(year);
   console.log(state);
-  var url = 'https://wildfire-api-3.herokuapp.com/api/v1.0/interactive_pie/' + state + '/' + year;
-  d3.json(url).then((data) => {
-    console.log(data);
+  var url = 'https://datavisproject2.herokuapp.com/api/v1.0/interactive_pie/' + state + '/' + year;
+  console.log(url);
+  await d3.json(url).then(data=>console.log(data));
 
-})};
+  // Create the plots based on selections
+  BuildPie(state, year);
 
-// function BuildPie(state, year) {
+};
 
 
-//   // var state = data.map(row => row[8]);
-//   // var year = data.map(row => row[9]);
+async function BuildPie(state, year) {
 
-//   // Grab values from the response csv object to build the plots
+  var state = document.getElementById('dropDown1').value;
+  var year = document.getElementById('dropDown2').value;
+  console.log(year);
+  console.log(state);
+  var url = 'https://datavisproject2.herokuapp.com/api/v1.0/interactive_pie/' + state + '/' + year;
+  console.log(url);
+  var data = await d3.json(url);
+  console.log(data)
+
+
+  // Grab values from the response csv object to build the plots
   
-//   var cause1 = data.map(row => row[0]);
+  var cause1 = Object.values(data[0])[0]
+  data.map(x => x[cause1]);
+  console.log(cause1);
 
-//   // Sort and count each cause in cause1
-//   var statistics = Object.keys(data).reduce(function (stats, key) {
-//     var item = data[key];
-//     if (item.cause1 === 'Natural') {
-//       stats.Natural += 1;
+  var state = Object.values(data[0])[8]
+  data.map(x => x[state]);
+  console.log(state);
+
+
+  // Sort and count each cause in cause1
+  var statistics = Object.values(data).reduce(function (stats, key) {
+    var item = data[key];
+    if (item.cause1 === 'Natural') {
+      stats.Natural += 1;
   
-//     if (item.cause1 === 'Human') {
-//         stats.Human += 1;
+    if (item.cause1 === 'Human') {
+        stats.Human += 1;
 
-//     if (item.cause1 === 'Unknown') {
-//         stats.Unknown += 1;
-//       }
-//     }
-//   }
-//     return stats;
-//   }, { Natural: 0, Human: 0, Unknown: 0 });
+    if (item.cause1 === 'Unknown') {
+        stats.Unknown += 1;
+      }
+    }
+  }
+    return stats;
+  }, { Natural: 0, Human: 0, Unknown: 0 });
   
-//   console.log('statistics : ', statistics);
+  console.log('statistics : ', statistics);
 
+  // Create trace for pie plot
+  var trace1 = {
+    type: "pie",
+    values: statistics,
+    labels: cause1,
+  };
 
-//   // function counter(h, n, u) {
-//   //   return json.export.filter(function(elem) {
-//   //     return elem.human===h && elem.natural===n && elem.unknown===u;
-//   //   }).length;
-//   // }
+  var data = [trace1];
 
-//   // Create trace for pie plot
-//   var trace1 = {
-//     type: "pie",
-//     values: statistics,
-//     labels: cause1,
-//   };
+  var layout = {
+    title: `${state}`,
+    height: 600,
+    width: 800
+    }
 
-//   var data = [trace1];
+  Plotly.newPlot("plot", data, layout);
+};
 
-//   var layout = {
-//     title: `${state}`,
-//     height: 600,
-//     width: 800
-//     }
-
-//   Plotly.newPlot("plot", data, layout);
-// };
 
 // Add event listener for submit button
-d3.selectAll("body").on("change", CreateURL);
+d3.selectAll("body").on("click", CreateURL);
 
-// Create one drop downs
-// function GetURL(){
-//   var state = document.getElementById('dropDown1').value;
-//   var year = document.getElementById('dropDown2').value;
-//   var url = 'https://wildfire-api-3.herokuapp.com/api/v1.0/interactive_pie/' + state + '/' + year;
-  
-//   d3.json(url).then((data) => {
-//     console.log(data);
-//     console.log(url);
 
-//     var dropdown = d3.select("#dropDown1");
-    
-//     data.st.forEach(function(state) {
-//       dropdown.append("option").text(st).property("value");
-//     });
 
-//   // Create the plots based on the first state
-//   let state = data.st[0];
-//   // Create the plots based on state
-//   BuildPie(state);
-
-// })};
-
+  // function counter(h, n, u) {
+  //   return json.export.filter(function(elem) {
+  //     return elem.human===h && elem.natural===n && elem.unknown===u;
+  //   }).length;
+  // }
